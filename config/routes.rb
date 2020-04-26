866b79df910d6e86bb5302e2cd9141abaf2e11b7
iqre8 <g.v.tjongahung@iqreate.com>
2020-04-26 15:47:30 -0300

    Added authorizer and CORS

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Jets.application.routes.draw do
   resources :posts
   root "jets/public#show"
-
+  #Doesn't work
+  #get "posts", to: "posts#index", authorizer: "main#my_cognito"
+  #Works with authorizer "main#my_cognito", only: %w[index] in posts_controller.rb
+  get "posts", to: "posts#index"
   # The jets/public#show controller can serve static utf8 content out of the public folder.
   # Note, as part of the deploy process Jets uploads files in the public folder to s3
   # and serves them out of s3 directly. S3 is well suited to serve static assets.
